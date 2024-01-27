96feac1bcd7b78ce7d04f4559293444533c7c764
Tristan <tristanjamesduke@gmail.com>
2024-01-27 00:37:56 -0400

    Added playercharacter and jump scripts

--- Comic Relief/Assets/Assets/Scripts/PlayerController.cs
+++ Comic Relief/Assets/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Camera _mainCamera;
+    
+    public Transform player;
+    
+    private Vector2 _moveInput;
+    public float moveSpeed = 4;
+
+    public Rigidbody2D rigidBody;
+
     void Start()
     {
-        
+        _mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        //_moveInput.y = Input.GetAxisRaw("Vertical");
+
+        _moveInput.Normalize();
+        rigidBody.velocity = _moveInput * moveSpeed;
+        rigidBody.freezeRotation = true;
     }
 }
