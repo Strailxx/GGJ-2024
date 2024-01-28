8d258b810f09a5489201c766cd75b58563d9763c
Brady02 <72949888+Brady02@users.noreply.github.com>
2024-01-28 16:03:19 -0400

    ahhhhhh

---
+++ Comic Relief/Assets/Assets/Scripts/GiveHatScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveHatScript : MonoBehaviour
+{
+    public InventorySystem inv;
+    public GameObject Hat;
+    public GameObject Hat1;
+    public GameObject Hat2;
+    public GameObject Hat3;
+
+    public GameObject DestHat;
+
+    public bool StarHasHat = false;
+
+    public bool inPosition;
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && inPosition)
+        {
+            DestHat.SetActive(true);
+            inv.Remove(Hat);
+            inv.Remove(Hat1);
+            inv.Remove(Hat2);
+            inv.Remove(Hat3);
+            StarHasHat = true;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && inv.items.Contains(Hat) || inv.items.Contains(Hat1) || inv.items.Contains(Hat2) || inv.items.Contains(Hat3))
+        {
+            inPosition = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inPosition = false;
+        }
+    }
+}
